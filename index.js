c4e763e153dd31c1b384f001dd3e4825570af7eb
david friedman <mrdavidfriedman@gmail.com>
2017-08-31 16:08:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ function nowServing(katzDeliLine) {
     return `Currently serving ${name}.`;
   }
 }
+
+function currentLine(line) {
+  if (line.length==0) {
+    return 'The line is currently empty.'
+  } else {
+    let message = 'The line is currently ';
+    for (let i=0;i<line.length;i++) {
+      message += `${i}. line[i]`;
+    }
+    return message;
+  }
+}
