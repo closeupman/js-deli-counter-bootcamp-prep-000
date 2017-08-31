8e1b486705dde8779e05a58d2497a3ff24634f19
david friedman <mrdavidfriedman@gmail.com>
2017-08-31 15:18:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length ==0) {
-    return 'The line is empty';
+    return 'There is nobody waiting to be served!';
   } else {
     let name = katzDeliLine.shift();
-    return `Now serving ${name}`;
+    return `Currently serving ${name}.`;
   }
 }
