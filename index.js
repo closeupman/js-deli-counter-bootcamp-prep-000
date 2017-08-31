a2cfd6ba4d2fe6fb65293961af77db15688fc24e
david friedman <mrdavidfriedman@gmail.com>
2017-08-31 17:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ function currentLine(line) {
     return 'The line is currently empty.'
   } else {
     let message = 'The line is currently: ';
-    for (let i=0;i<line.length;i++) {
+    for (let i=0;i<line.length-1;i++) {
       message += `${i+1}. ${line[i]}, `;
     }
+    message+=`${line.length-1}. ${line[line.length-1]}`
     return message;
   }
 }
